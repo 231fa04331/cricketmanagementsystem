16cb134fa12cd938b6fac96d1b2c886bda035c3f
231fa04331 <charanpadarthi1430@gmail.com>
2025-08-09 18:15:41 +0530

    code added in main

--- src/main/java/main/PlayerMain.java
+++ src/main/java/main/PlayerMain.java
@@ -1,5 +1,14 @@
 package main;
 
-public class PlayerMain {
+import controller.PlayerController;
+import model.PlayerDatabase;
+import view.PlayerView;
 
+public class PlayerMain {
+    public static void main(String[] args) {
+        PlayerDatabase db = new PlayerDatabase();
+        PlayerView view = new PlayerView();
+        PlayerController controller = new PlayerController(db, view);
+        controller.start();
+    }
 }
